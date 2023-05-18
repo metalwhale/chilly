4d1ebf18fee6272cfa71439d49481b1f50800805
Metal Whale <metalwhale.dev@gmail.com>
2023-05-18 11:17:30 +0000

    Add Docker. Format.

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.7
+
+COPY requirements.txt /tmp/requirements.txt
+RUN pip3 install -r /tmp/requirements.txt \
+    && rm /tmp/requirements.txt
--- train.py
+++ train.py
@@ -1,3 +1,18 @@
+import glob
+import os
+import random
+import re
+import shutil
+import json
+import zipfile
+from collections import defaultdict
+from typing import List
+
+import torch
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
+from transformers import LlamaForCausalLM, LlamaTokenizer, DataCollatorForSeq2Seq, Trainer, TrainingArguments
+
 BASE_MODEL = "decapoda-research/llama-7b-hf"
 ZIP_FILE = "data/slack-export-Mart6-2018-Mar31-2023.zip"
 RAW_DATA_DIR = "data/raw_data"
@@ -6,40 +21,33 @@ VAL_FILE = "data/val.json"
 OUTPUT_DIR = "data/output"
 TRAIN_LENGTH = 16000
 VAL_LENGTH = 2000
-EPOCHS = 4
-
-import shutil
-import zipfile
-
+EPOCHS = 1
 shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 shutil.rmtree(RAW_DATA_DIR, ignore_errors=True)
 with zipfile.ZipFile(ZIP_FILE, "r") as zip_file:
     zip_file.extractall(RAW_DATA_DIR)
 
-import os
-import glob
-import json
-import re
-import random
-from collections import defaultdict
-from typing import List
 
 class Message:
     text: str
     ts: str  # timestamp
     user: str
+
     def __init__(self, text: str, ts: str, user: str) -> "Message":
         self.text = text
         self.ts = ts
         self.user = user
 
+
 class Conversation:
     messages: List[Message]
+
     def __init__(self) -> "Conversation":
         self.messages = []
 
+
 def generate_dataset(input_dir: str):
-    CONVERSATION_DISTANCE_TIME = 15 * 60  # in seconds
+    CONVERSATION_DISTANCE_TIME = 15 * 60  # In seconds
     CONVERSATION_MIN_LENGTH = 2
     conversations: list[Conversation] = []
     for channel in next(os.walk(input_dir))[1]:  # List all subdirectories
@@ -84,14 +92,7 @@ def generate_dataset(input_dir: str):
                 data_file, ensure_ascii=False, indent=4,
             )
 
-generate_dataset(RAW_DATA_DIR)
 
-import json
-from datasets import load_dataset
-from transformers import LlamaTokenizer
-
-tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
-tokenizer.pad_token_id = 0
 def load_data(file_path):
     def tokenize(d):
         encoding = tokenizer(d["text"], truncation=True, max_length=256)
@@ -100,15 +101,14 @@ def load_data(file_path):
     data = load_dataset("json", data_files=file_path)
     tokenized_data = data["train"].shuffle().map(tokenize)
     return tokenized_data
+
+
+generate_dataset(RAW_DATA_DIR)
+tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+tokenizer.pad_token_id = 0
 train_data = load_data(TRAIN_FILE)
 val_data = load_data(VAL_FILE)
-
 print(len(train_data), len(val_data))
-
-import torch
-from transformers import LlamaForCausalLM
-from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
-
 model = LlamaForCausalLM.from_pretrained(
     BASE_MODEL,
     load_in_8bit=True,
@@ -126,9 +126,6 @@ config = LoraConfig(
 )
 model = get_peft_model(model, config)
 model.print_trainable_parameters()
-
-from transformers import Trainer, TrainingArguments, DataCollatorForSeq2Seq
-
 trainer = Trainer(
     model=model,
     train_dataset=train_data,
